6102826822984d196a355d1e4a8dcd6c559f9674
Carina Ponce <carina_ponce@uadec.edu.mx>
2023-05-19 17:27:14 -0600

    Se modifico el ingreso de los medicamentos, se agrego una lista donde se van agregando los id de los medicamentos para luego mandarla llamar para la consulta

--- pastillero_control.py
+++ pastillero_control.py
@@ -10,15 +10,15 @@ class ProgramaPrincipal(Ui_MainWindow, QtWidgets.QMainWindow, QtWidgets.QLineEdi
         # Iniciar el programa en la primera pestaña
         self.tabWidget.setCurrentIndex(0)
         self.tab_inicio.setEnabled(True)
-        self.tab_inicioSesion.setEnabled(False)
-        self.tab_registro_medicamento.setEnabled(False)
-        self.tab_registro_px.setEnabled(False)
-        self.tab_tabla_px.setEnabled(False)
+      #  self.tab_inicioSesion.setEnabled(False)
+      #  self.tab_registro_medicamento.setEnabled(False)
+      #  self.tab_registro_px.setEnabled(False)
+      #  self.tab_tabla_px.setEnabled(False)
         # Eventos con sus slots 
         self.btn_busqueda.clicked.connect(self.busquedaTab)
         self.btn_registrarce.clicked.connect(self.registroPxTab)
         self.btn_registrar_px.clicked.connect(self.regMedTab)
-        self.btn_end_med.clicked.connect(self.busquedaTab)
+        self.btn_end_med.clicked.connect(self.pastilleroView)
         self.btn_pxView.clicked.connect(self.pastilleroView)
         self.btn_back_inicio.clicked.connect(self.regresarMenuPrincipal)
         self.btn_add_med.clicked.connect(self.addMedicamento)
@@ -27,6 +27,18 @@ class ProgramaPrincipal(Ui_MainWindow, QtWidgets.QMainWindow, QtWidgets.QLineEdi
         self.btn_updateBox.clicked.connect(self.rellenarCBox)
         self.btn_regresar_main.clicked.connect(self.regresarMenuPrincipal)
         self.btn_regresar_2.clicked.connect(self.regresarMenuPrincipal)
+        self.lEdit_nombre_reg.textChanged.connect(self.limpiaLineEdit)
+        self.lEdit_Dx.textChanged.connect(self.limpiaLineEdit)
+        self.lEdit_User.textChanged.connect(self.limpiaLineEdit)
+        self.lEdit_Password.textChanged.connect(self.limpiaLineEdit)
+        self.lEdit_edad_reg.textChanged.connect(self.limpiaLineEdit)
+        self.lEdit_NoSS.textChanged.connect(self.limpiaLineEdit)
+        self.rellenarCBox()
+        self.user = "juancho"
+        self.lista_medicamentos = []
+
+    def limpiaLineEdit(self):
+        self.lbl_corregir.setText("")
 
     def registroPxTab(self):
         # Cambio a pestaña de registro PX
@@ -45,9 +57,15 @@ class ProgramaPrincipal(Ui_MainWindow, QtWidgets.QMainWindow, QtWidgets.QLineEdi
     def addMedicamento(self):
         # Aqui se seleccionas los medicamentos del combobox y 
         # los guarda en la base de datos de los medicamentos que toma el paciente
-        self.tab_registro_medicamento.setEnabled(True)
-        self.NombreMedicamento = self.comboBox.currentText()
-        print(self.NombreMedicamento)
+        if len(self.lista_medicamentos) < 5:
+            self.NombreMedicamento = self.comboBox.currentText()
+            self.queryMed = query.Medicamento(self.NombreMedicamento)
+            self.IDMed = self.queryMed.medQuery2()
+            self.lista_medicamentos.append(self.IDMed)
+            print(self.lista_medicamentos)
+            self.label_9.setText(f"Medicamentos seleccionados: {str(len(self.lista_medicamentos))}")
+        else:
+            self.label_9.setText(f"Medicamentos seleccionados: {str(len(self.lista_medicamentos))} \n Ya no es posible agregar mas medicamentos")
 
     def modificar(self):
         self.tabWidget.setCurrentIndex(2)
@@ -58,7 +76,6 @@ class ProgramaPrincipal(Ui_MainWindow, QtWidgets.QMainWindow, QtWidgets.QLineEdi
         self.tab_inicioSesion.setEnabled(True)
         self.tab_inicio.setEnabled(False)
 
-    
     def addNewMEd(self):
         # Cuando se quiere agregar un medicamento que no esta en la lisa
         # se abre una nueva pestaña para agregarlo
@@ -66,41 +83,61 @@ class ProgramaPrincipal(Ui_MainWindow, QtWidgets.QMainWindow, QtWidgets.QLineEdi
 
     def regMedTab(self):
         # Captura de los datos del px de los line edit
-        self.edad = 0
-        self.NoSS = 0
-        try:
-            self.edad = int(self.lEdit_edad_reg.text())
-            self.NoSS = int(self.lEdit_NoSS.text())
-        except:
-            self.lbl_corregir.setText("La edad y el No. Seguro social deben ser con numero")
-        
-        self.nombrePx = self.lEdit_nombre_reg.text()
-        self.padecimiento = self.lEdit_Dx.text()
-        self.user = self.lEdit_User.text()
-        self.password = self.lEdit_Password.text()
-
-        try:
-            # Insertat los datos en la base de datos
-            self.RegistroPaciente = query.Paciente(self.nombrePx, self.padecimiento, self.edad, self.NoSS, self.user, self.password)
-            self.RegistroPaciente.registroPX()
-            # Cambio a pestaña info medicameto
-            self.tabWidget.setCurrentIndex(2)
-            self.tab_registro_medicamento.setEnabled(True)
-            self.tab_registro_px.setEnabled(False)
-            # Limpiar lineedit
-        except(ValueError):
-            self.lbl_corregir.setText("Ingrese valores corretos")
-
-        finally:
-            # Rellenar la combobox con los medicamentos existentes en la BD
-            self.rellenarCBox()
+        line_edits = [self.lEdit_nombre_reg, self.lEdit_Dx, self.lEdit_User, self.lEdit_Password, self.lEdit_edad_reg, self.lEdit_NoSS]
+        if all(line_edit.text() for line_edit in line_edits):
+            self.nombrePx = self.lEdit_nombre_reg.text()
+            self.padecimiento = self.lEdit_Dx.text()
+            self.user = self.lEdit_User.text()
+            self.password = self.lEdit_Password.text()
+            try:
+                self.edad = int(self.lEdit_edad_reg.text())
+                self.NoSS = int(self.lEdit_NoSS.text())
+            except:
+                self.lbl_corregir.setText("*Ingrese la edad  y el numero de servicio social en numero*")
+            else:
+                try:
+                    # Insertar los datos en la base de datos
+                    self.RegistroPaciente = query.Paciente(self.nombrePx, self.padecimiento, self.edad, self.NoSS, self.user, self.password)
+                    self.RegistroPaciente.registroPX()
+                    # Cambio a pestaña info medicameto
+                    self.tabWidget.setCurrentIndex(2)                    
+                    self.tab_registro_medicamento.setEnabled(True)
+                    self.tab_registro_px.setEnabled(False)
+                    # Limpiar lineedit
+                    for line in line_edits:
+                        line.clear()
+                except:
+                    self.lbl_corregir.setText("Error en la insersion de datos")
+        else:
+            self.lbl_corregir.setText("*Agregar correctamente los campos*")
                 
     def pastilleroView(self):
         # Cambio a la pestaña de visualizar los medicamentos
         self.tabWidget.setCurrentIndex(4)
         self.tab_tabla_px.setEnabled(True)
         self.tab_inicioSesion.setEnabled(False)
-    
+        self.tab_registro_medicamento.setEnabled(False)
+
+        self.id2 = self.busqueda_id_usuario()
+
+        # Si se seleccionaron menos de 5 medicamentos, se rellenan los espacios
+        self.faltantes = 5 - len(self.lista_medicamentos) # verificar cuantos elementos tiene la lista
+        if self.faltantes > 0:
+            for n in range(5-self.faltantes, 5): # verificar de que indice empezar
+                self.lista_medicamentos.append("1")
+        print(self.lista_medicamentos)
+
+        # Insertar los medicmamentos en la tabla tratamiento
+        self.queryTratamiento = query.Tratamiento(self.lista_medicamentos, self.id2)
+        self.queryTratamiento.insertTrat()
+
+    def busqueda_id_usuario(self):
+    # Busqueda del id del paciente
+        self.queryID = query.Paciente(User=self.user)
+        self.queryID_PX = self.queryID.consultaIdPX()
+        self.queryID_PX = self.queryID_PX[0]
+        return self.queryID_PX
+
     def rellenarCBox(self):
         self.rellenar = query.Medicamento(5, "medicamento")
         self.records = self.rellenar.medQuery()
@@ -112,7 +149,7 @@ class ProgramaPrincipal(Ui_MainWindow, QtWidgets.QMainWindow, QtWidgets.QLineEdi
                 self.comboBox.addItem(registro[1])
             # Si ya existe dejamos pasar
             else:
-                print(self.busqueda)
+                pass
 
 class Ui_Form(Ui_Form, QtWidgets.QMainWindow):
     def __init__ (self, *args, **kwargs):
@@ -129,7 +166,7 @@ class AgregarMedicamento(Ui_Dialog, QtWidgets.QDialog, QtWidgets.QLineEdit):
         # Aqui se guarda el nuevo medicamento a la base de datos desde el Qdialog
         self.medicamento = self.lEdit_Medicamento.text()
         self.id_med = self.medicamento[1:4].upper() + "1"
-        self.agregar = query.Medicamento(self.id_med, self.medicamento)
+        self.agregar = query.Medicamento(id_medicamento = self.id_med, medicamento = self.medicamento)
         self.lbl__resultado.setText(self.agregar.registroMedicamento())
 
 if __name__=="__main__":
--- query.py
+++ query.py
@@ -2,19 +2,23 @@
 import psycopg2
 
 # En esta clase se guardara el nombre del medicamento en la base de datos
-class Medicamento:
-    def __init__(self, id_medicamento, medicamento):
-        self._idMedicamento = id_medicamento
-        self._medicamento = medicamento
+class CONEXION:
+    def __init__(self):        
         try:
             self.conexion = psycopg2.connect(
                 user = 'postgres', 
                 password = '18228871', 
                 host =  '127.0.0.1', 
                 database = 'Pastillero')
-            print("Conexion exitosa con base de datos pastillero")
+           # print("Conexion exitosa con base de datos pastillero")
         except Exception:
             print(f"Ha ocurrido un error con la conexion a la base de datos")
+
+class Medicamento(CONEXION):
+    def __init__(self, medicamento, id_medicamento = ""):
+        super().__init__()
+        self._idMedicamento = id_medicamento
+        self._medicamento = medicamento
     
     def registroMedicamento(self):
         try:
@@ -22,9 +26,9 @@ class Medicamento:
                 with self.conexion.cursor() as self.cursor:
                     self.consulta = "INSERT INTO \"Medicamentos\"(id_medicamento, nombre_medicamento) VALUES(%s, %s)"
                     self.valores = self._idMedicamento, self._medicamento
-                    self.cursor.execute(self.consulta,self.valores)
-                    self.regafectados = self.cursor.rowcount
-                    return f"Medicamentos agregados {self.regafectados}"      
+                    self.cursor.execute(self.consulta, self.valores)
+                    self.reg_afectados = self.cursor.rowcount
+                    return f"Medicamentos agregados {self.reg_afectados}"      
         except psycopg2.errors.UniqueViolation:
             return "¡El medicamento que ya esta registrado!"
         except Exception:
@@ -36,32 +40,38 @@ class Medicamento:
         try:
             with self.conexion:
                 with self.conexion.cursor() as self.cursor:
-                    self.consulta = "SELECT * FROM \"Medicamentos\" ORDER BY id_medicamento"
+                    self.consulta = f"SELECT * FROM \"Medicamentos\" ORDER BY nombre_medicamento ASC"
                     self.cursor.execute(self.consulta)
                     self.registros  = self.cursor.fetchall()
-                    return self.registros
-        
+                    return self.registros        
         except:
-            print("Ha ocurrido un error en la consulta")
+            print("Ha ocurrido un error en la consulta de los medicamentos")
+        finally:
+           self.cursor.close()
+    
+    def medQuery2(self):
+        try:
+            with self.conexion:
+                with self.conexion.cursor() as self.cursor:
+                    self.consulta = f'SELECT id_medicamento FROM \"Medicamentos\" WHERE nombre_medicamento = \'{self._medicamento}\''
+                    self.cursor.execute(self.consulta)
+                    self.registros  = self.cursor.fetchone()
+                    return self.registros[0]        
+        except:
+            print("Ha ocurrido un error en la consulta del id medicamentos")
+        finally:
+           self.cursor.close()
 
 # Clase para registrar al paciente
-class Paciente:
-    def __init__(self, nombre, enfermedad, edad, NoSS, User, password):
+class Paciente(CONEXION):
+    def __init__(self, nombre = "val1", enfermedad = "val2", edad = "val3", NoSS = "val4", User = "val5", password = "val6"):
+        super().__init__()
         self._nombre = nombre
         self._enfermedad = enfermedad
         self._edad = edad
         self._SeguroSocial = NoSS
         self._usuario = User
         self._password = password
-        try:
-            self.conexion = psycopg2.connect(
-                user = 'postgres',
-                password = '18228871',
-                host = "127.0.0.1",
-                database = 'Pastillero'
-            )
-        except:
-            print("Conexion incorrecta")
         
     def registroPX(self):
         try:
@@ -69,7 +79,7 @@ class Paciente:
                 with self.conexion.cursor() as self.cursor:
                     self.consulta = "INSERT INTO public.\"DATOS_PX\"(\"NOMBRE\", \"Enfermedad\", \"Edad\", \"NoSS\", \"Usuario\", \"Contra\") VALUES (%s, %s, %s, %s, %s, %s)"
                     self.values  =  (self._nombre, self._enfermedad, self._edad, self._SeguroSocial, self._usuario, self._password)
-                    self.cursor.execute(self.consulta,self.values)
+                    self.cursor.execute(self.consulta, self.values)
                     self.regafectados = self.cursor.rowcount
                     print(f"Pacientes agregados {self.regafectados}")
         except:
@@ -77,9 +87,38 @@ class Paciente:
         finally:
             self.cursor.close()
 
-    def consultaPX(self):
-        pass       
+    def consultaIdPX(self):
+        try:
+            with self.conexion:
+                with self.conexion.cursor() as self.cursor:
+                    self.consulta = f'SELECT \"ID_PX\" FROM public.\"DATOS_PX\" WHERE \"Usuario\" = \'{self._usuario}\''
+                    self.cursor.execute(self.consulta)
+                    self.registros  = self.cursor.fetchone()
+                    return self.registros 
+        except:
+            print("Ha ocurrido un error en la consulta del Id del paciente")       
+        finally:
+            self.cursor.close()   
 
+class Tratamiento(CONEXION):
+    def __init__(self, nombre_medicamento, id_usuario):
+        super().__init__()
+        self._nombre_medicamento = tuple(nombre_medicamento)
+        self._numMedicamento = len(nombre_medicamento)
+        self._id_usuario = id_usuario
+    
+        self.values = (self._id_usuario,) + self._nombre_medicamento
+        print(self.values)
+    def insertTrat(self):
+        try:
+            with self.conexion:
+                with self.conexion.cursor() as self.cursor:
+                    self.consulta = "INSERT INTO \"TRATAMIENTO\"(\"ID_PACIENTE\", \"ID_MED_1\", \"ID_MED_2\", \"ID_MED_3\", \"ID_MED_4\", \"ID_MED_5\") VALUES(%s, %s, %s, %s, %s, %s)"
+                    self.cursor.execute(self.consulta, self.values)
+                    self.registros_afectados = self.cursor.rowcount
+                    print(f'Registros afectados {self.registros_afectados}')
+        except:
+            print("Fallo en la insersion de datos del tratamiento")
 if __name__ == "__main__":
     a = Paciente('Diana', 'Ninguna', 23, 555, 'ponce', 'carinita')
     a.registroPX()
